1983174d0a41a8db8be88e8dc20d343758f5d583
vinodk11 <vinod.k118888@gmail.com>
2026-05-03 19:24:15 +0530

    modified: eks_cluster/iam_auth.tf2

--- eks_cluster/iam_auth.tf
+++ eks_cluster/iam_auth.tf
@@ -19,7 +19,7 @@ EOF
 
   depends_on = [
     aws_eks_cluster.my_eks_cluster,
-    aws_iam_role.eks_admin_role,
+    #aws_iam_role.eks_admin_role,
     aws_iam_role.ebs_csi_role,
     aws_iam_openid_connect_provider.eks_oidc_provider,
   ]
