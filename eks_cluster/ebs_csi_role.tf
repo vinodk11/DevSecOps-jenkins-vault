cbe98f52d46bc3e020190a60a9f8594d7fc0a81a
vinodk11 <vinod.k118888@gmail.com>
2026-05-03 15:04:31 +0530

    modified: eks_cluster/variables.tf3 & ebs_csi_role.tf

--- eks_cluster/ebs_csi_role.tf
+++ eks_cluster/ebs_csi_role.tf
@@ -1,22 +1,21 @@
 resource "aws_iam_role" "ebs_csi_role" {
-    name = "ebs-csi-role"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                effect = "Allow"
-                Principal = {
-                    Service = "ec2.amazonaws.com"
-                }
-                Action = "sts:AssumeRole"
-            }
-        ]
-    })
-  
+  name = "ebs-csi-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    role = aws_iam_role.ebs_csi_role.name
-  
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_role.name
 }
