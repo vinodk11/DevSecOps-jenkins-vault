2ed32c57e51013304d9f5ec8aedd4cb60ee35dc7
vinodk11 <vinod.k118888@gmail.com>
2026-05-03 19:53:57 +0530

    modified: eks_cluster/provider.tf

--- eks_cluster/provider.tf
+++ eks_cluster/provider.tf
@@ -19,3 +19,22 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+
+
+  data "aws_eks_cluster" "cluster" {                                                                                                  
+    name = aws_eks_cluster.my_eks_cluster.name                                                                                        
+  }                                                                                                                                   
+                                                                                                                                      
+  data "aws_eks_cluster_auth" "cluster" {                                                                                             
+    name = aws_eks_cluster.my_eks_cluster.name                                                                                        
+  }                                                                                                                                   
+                                                                                                                                      
+  provider "kubernetes" {                                                                                                             
+    host                   = data.aws_eks_cluster.cluster.endpoint                                                                    
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)                                 
+    token                  = data.aws_eks_cluster_auth.cluster.token                                                                  
+                                                                                                                                      
+    # optional: set a short request timeout to avoid hangs                                                                            
+    request_timeout        = 60                                                                                                       
+  }                                                                                                                                   
