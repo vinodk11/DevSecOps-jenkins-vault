cec0fb0e32692d5891f661d0022d468ef5bebcaa
vinodk11 <vinod.k118888@gmail.com>
2026-05-06 18:47:19 +0530

    modified:eks_cluster/provider.tf

--- eks_cluster/provider.tf
+++ eks_cluster/provider.tf
@@ -22,7 +22,7 @@ provider "aws" {
 
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster..my_eks_cluster.name
+  name = aws_eks_cluster.my_eks_cluster.name
 }
 
 provider "kubernetes" {
