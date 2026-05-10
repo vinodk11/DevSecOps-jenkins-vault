db760f1e8844c709873e5ba307e43199a6f5c28f
vinodk11 <vinod.k118888@gmail.com>
2026-05-10 17:07:01 +0530

    modified:eks_cluster/provider.tf

--- eks_cluster/provider.tf
+++ eks_cluster/provider.tf
@@ -32,3 +32,15 @@ provider "kubernetes" {
   )
   token = data.aws_eks_cluster_auth.cluster.token
 }
+
+provider "helm" {
+  kubernetes {
+    host = aws_eks_cluster.my_eks_cluster.endpoint
+
+    cluster_ca_certificate = base64decode(
+      aws_eks_cluster.my_eks_cluster.certificate_authority[0].data
+    )
+
+    token = data.aws_eks_cluster_auth.cluster.token
+  }
+}
